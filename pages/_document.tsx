3bf86ec7f3d75c7efdd92ef46c736167d07e357e
Eko Susilo <ekosusilo140604@gmail.com>
2023-03-16 15:53:40 +0700

    feat: add title and favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,8 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500&family=Inter:wght@400;600&family=Source+Sans+Pro:wght@400;600&display=swap"
           rel="stylesheet"
         />
+        <title>CHAT N’ RECHAT</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
       </Head>
       <body>
         <Main />
